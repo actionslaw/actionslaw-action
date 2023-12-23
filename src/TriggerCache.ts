aafbd5bbdbc85353a48ddf54faabab650365d20d
R.G. Wood <ric@grislyeye.com>
2023-12-23 19:07:06 +0000

    Fix: duplicate triggers not being ignored

--- src/TriggerCache.ts
+++ src/TriggerCache.ts
@@ -1,15 +1,13 @@
 import { Key } from "./triggers/Trigger";
 import * as cache from "@actions/cache";
 import * as fs from "fs";
-import * as fsExtra from "fs-extra";
 
 export class TriggerCache {
   private static readonly file: string = "./actionslaw.cache.json";
 
   static async isCached(key: Key): Promise<boolean> {
     console.debug(`🗺️  Checking trigger cache [${key}:${TriggerCache.file}]`);
-    await cache.restoreCache([TriggerCache.file], key);
-    return await fsExtra.pathExists(TriggerCache.file);
+    return await cache.restoreCache([TriggerCache.file], key) !== undefined;
   }
 
   static async save(tocache: Key[]): Promise<void> {
