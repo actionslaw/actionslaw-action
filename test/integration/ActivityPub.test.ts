a8b9bf0b402cbc1c97b161b6c3f892bb1b7df886
R.G. Wood <ric@grislyeye.com>
2024-01-14 13:33:44 +0000

    Fix: broken test

--- test/integration/ActivityPub.test.ts
+++ test/integration/ActivityPub.test.ts
@@ -224,7 +224,7 @@ describe("ActivityPub should", () => {
 
     const posts = await trigger.run();
 
-    expect(posts.find((p) => p.downloads!.includes(attachment))).toBeTruthy();
+    expect(posts.find((p) => p.media!.includes(attachment))).toBeTruthy();
   });
 });
 
