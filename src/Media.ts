1dc227473f852f1c4bf40b2efaf6d2fa4e8d8199
R.G. Wood <ric@grislyeye.com>
2024-12-22 13:35:32 +0000

    Tidy whitespace

--- src/Media.ts
+++ src/Media.ts
@@ -15,13 +15,17 @@ export class Media {
         .map((url) => new URL(url))
         .map(async (mediaUrl) => {
           const media = await fetch(mediaUrl);
+
           if (!fs.existsSync(Media.folder)) fs.mkdirSync(Media.folder);
+
           const fileName = path.basename(mediaUrl.pathname);
           const destination = path.resolve(`./${Media.folder}`, fileName);
           const fileStream = fs.createWriteStream(destination, { flags: "w" });
+
           if (media.body) {
             await finished(Readable.fromWeb(media.body).pipe(fileStream));
           }
+
           return destination;
         }),
     );
@@ -30,7 +34,9 @@ export class Media {
   static async cache(key: string, urls: string[]): Promise<void> {
     if (urls.length > 0) {
       const files = await Media.download(urls);
+
       core.info(`🔫 caching media ${files} for key ${key}`);
+
       await cache.saveCache([`${Media.folder}/**`], key);
       fs.rmSync(Media.folder, { recursive: true, force: true });
     }
